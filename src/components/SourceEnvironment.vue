4451f9b410bf3e3729633777f635d4a288210c55
Matthias Mohr <m.mohr@uni-muenster.de>
2018-03-08 14:53:59 +0100

    Moved the default script to the config file.

--- src/components/SourceEnvironment.vue
+++ src/components/SourceEnvironment.vue
@@ -40,11 +40,7 @@ export default {
 				}
 			},
 			editor: null,
-			defaultScript: `// Create the process graph
-OpenEO.Editor.ProcessGraph = OpenEO.ImageCollection.create('Sentinel2A-L1C')
-	.filter_daterange("2018-01-01","2018-01-31")
-	.NDVI("B04","B08")
-	.max_time();`
+			defaultScript: this.$config.defaultScript
 		}
 	},
 	mounted() {
--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
 export default {
 
 	serverUrl: 'http://localhost:8000/',
-	allowServerChange: true
-
+	allowServerChange: true,
+	defaultScript: `// Create the process graph
+OpenEO.Editor.ProcessGraph = OpenEO.ImageCollection.create('Sentinel2A-L1C')
+	.filter_daterange("2018-01-01","2018-01-31")
+	.NDVI("B04","B08")
+	.max_time();`
 };
