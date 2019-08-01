173557931612105c027fc7abf882ff791a253629
Matthias Mohr <m.mohr@uni-muenster.de>
2019-08-01 10:44:36 +0200

    Fixed elements that were shown, but had no function.

--- src/components/EditorToolbar.vue
+++ src/components/EditorToolbar.vue
@@ -8,7 +8,7 @@
 				<button v-if="enableLocalStorage && editable" type="button" @click="openScriptChooser" title="Load script from local storage"><i class="fas fa-folder-open"></i></button>
 				<button v-if="enableLocalStorage" type="button" @click="saveScript" title="Save script to local storage"><i class="fas fa-save"></i></button>
 			</span>
-			<button v-if="enableExecute" type="button" @click="executeProcessGraph" title="Run current process graph and view results" class="sepl"><i class="fas fa-play"></i></button>
+			<button v-if="enableExecute && supports('computeResult')" type="button" @click="executeProcessGraph" title="Run current process graph and view results" class="sepl"><i class="fas fa-play"></i></button>
 		</div>
 	</div>
 </template>
