e9513c81bfa5c9b828ccb2650cd6691181281f01
Matthias Mohr <m.mohr@uni-muenster.de>
2020-07-30 13:11:31 +0200

    Fix links to newer API version docs

--- src/components/modals/WebEditorModal.vue
+++ src/components/modals/WebEditorModal.vue
@@ -5,7 +5,7 @@
 			<p class="aboutPage">This software is published by the <strong>openEO Consortium</strong> under the <strong>Apache 2.0 license</strong>. Please find more information about the openEO project on our <a href="http://www.openeo.org" target="_blank">homepage</a> and visit the <a href="https://github.com/Open-EO/openeo-web-editor" target="_blank">GitHub repository</a> for information about the Web Editor. Feel encouraged to <strong>report bugs, feature requests and other issues in the <a href="https://github.com/Open-EO/openeo-web-editor/issues" target="_blank">GitHub issue tracker</a></strong>.</p>
 			<h3 class="aboutPage">Supported API versions:</h3>
 			<ul class="aboutPage">
-				<li v-for="v in pkg.apiVersions" :key="v"><a :href="'https://open-eo.github.io/openeo-api/v/'+v+'/index.html'" target="_blank">{{ v }}</a></li>
+				<li v-for="v in pkg.apiVersions" :key="v"><a :href="'https://api.openeo.org/'+v" target="_blank">{{ v }}</a></li>
 			</ul>
 		</template>
 	</Modal>
