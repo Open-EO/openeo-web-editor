f16cf621fea61f0047d29adb53a9ea37e89f1c29
Matthias Mohr <m.mohr@uni-muenster.de>
2021-03-15 18:45:03 +0100

    Implement collection items

--- src/components/modals/CollectionModal.vue
+++ src/components/modals/CollectionModal.vue
@@ -7,6 +7,17 @@
 						<MapViewer id="collectionMap" :show="showMap" :extents="p.extents"></MapViewer>
 					</template>
 				</Collection>
+				<section v-if="currentItems">
+					<Items :items="currentItems">
+						<template #item-location="p">
+							<MapViewer :id="String(Date.now())" :geoJson="p.geometry"></MapViewer>
+						</template>
+					</Items>
+					<div class="pagination">
+						<button title="Previous page" @click="paginate(-1)" :disabled="!hasPrevItems"><i class="fas fa-arrow-left"></i> Previous Page</button>
+						<button title="Next page" @click="paginate(1)" :disabled="!hasNextItems">Next Page <i class="fas fa-arrow-right"></i></button>
+					</div>
+				</section>
 			</div>
 		</template>
 	</Modal>
@@ -16,37 +27,77 @@
 import MapViewer from '../MapViewer.vue';
 import Modal from './Modal.vue';
 import Collection from '@openeo/vue-components/components/Collection.vue';
+import Utils from '../../utils.js';
 
 export default {
 	name: 'CollectionModal',
 	components: {
 		MapViewer,
 		Modal,
-		Collection
+		Collection,
+		Items: () => import('@openeo/vue-components/components/Items.vue')
 	},
 	data() {
 		return {
 			version: null,
 			collection: null,
-			showMap: false
+			showMap: false,
+			items: [],
+			itemsPage: 0,
+			itemsIterator: null
 		};
 	},
 	computed: {
+		...Utils.mapState(['connection']),
 		bbox() {
 			try {
 				return this.collection.extent.spatial.bbox[0];
 			} catch(e) {
 				return null;
 			}
+		},
+		currentItems() {
+			if (this.items.length >= this.itemsPage) {
+				return this.items[this.itemsPage];
+			}
+			return null;
+		},
+		hasPrevItems() {
+			return (this.itemsPage > 0);
+		},
+		hasNextItems() {
+			return (this.itemsPage < this.items.length - 1);
 		}
 	},
 	methods: {
-		show(collection) {
+		async paginate(step) {
+			if (step > 0) {
+				await this.nextItems();
+			}
+			else if (this.itemsPage === 0 && step < 0) {
+				return;
+			}
+
+			this.itemsPage += step;
+		},
+		async nextItems() {
+			if (!this.itemsIterator) {
+				this.itemsIterator = await this.connection.listCollectionItems(this.collection.id);
+			}
+			let next = await this.itemsIterator.next();
+			if (next && next.value && !next.done) {
+				this.items.push(next.value);
+			}
+		},
+		async show(collection) {
 			this.collection = collection;
 			this.$refs.modal.show(collection.id);
 			this.$nextTick(() => {
 				this.showMap = true;
 			});
+			await this.nextItems();
+			// Always request a page in advance so that we know whether a next page is available.
+			this.nextItems();
 		}
 	}
 }
@@ -71,6 +122,12 @@ export default {
 }
 .docgen h2 {
 	display: none;
+}
+.docgen .items h2 {
+	display: block;
+	font-size: 1.4em;
+    margin-top: 1.5em;
+    border-bottom-style: dotted;
 }
 #collectionMap {
 	height: 300px;
