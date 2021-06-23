cdce62e09ca5ad7933cb4329e0114dd8e3c2fec2
Matthias Mohr <m.mohr@uni-muenster.de>
2021-06-23 13:02:40 +0200

    Minor fixes

--- src/utils.js
+++ src/utils.js
@@ -28,6 +28,7 @@ class Utils extends VueUtils {
 				data: error,
 				links: error.links
 			};
+			// ToDo: This doesn't work if not authenticated / connected yet
 			buttons.push({
 				text: 'Show Details',
 				action: () => vm.emit('viewLogs', [log], 'Error', 'fa-bomb')
