13d8ae907f4c38d7a38d1fefe7610b52f0ce3473
openEO CI <openeo.ci@uni-muenster.de>
2021-07-23 12:08:28 +0000

    deploy: b301296a5e6d58b0ec893a0ec74655b17cf3d41f

--- README.md
+++
@@ -1,2 +0,0 @@
-# openeo-web-editor
-A web-based editor for interactive usage of the OpenEO API (proof-of-concept).
---
+++ js/chunk-76d54288.8f130922.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-76d54288"],{"6cc5":function(n,c,w){}}]);
+//# sourceMappingURL=chunk-76d54288.8f130922.js.map
