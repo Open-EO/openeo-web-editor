05845c9dbb2b0e68480d9a70bbd51ff0e15f8b96
Matthias Mohr <m.mohr@uni-muenster.de>
2022-05-20 16:42:25 +0200

    Allow to switch between grayscale and RGB in GeoTiff viewer

--- src/components/maps/ChannelControl.vue
+++ src/components/maps/ChannelControl.vue
@@ -4,7 +4,7 @@
 			<thead>
 				<tr>
 					<th v-if="!isGray">Channel</th>
-					<th v-if="!isGray">Band</th>
+					<th v-if="multiBand">Band</th>
 					<th>Min</th>
 					<th>Max</th>
 				</tr>
@@ -12,7 +12,7 @@
 			<tbody>
 				<tr v-for="(channel, i) in channels" :key="i">
 					<th v-if="!isGray">{{ labels[i] }}</th>
-					<td v-if="!isGray">
+					<td v-if="multiBand">
 						<select v-model.number="channel.id">
 							<option v-for="band in bands" :key="band.id" :value="band.id">{{ band.name || band.id }}</option>
 						</select>
@@ -21,6 +21,13 @@
 					<td><input type="number" v-model.number="channel.max" required /></td>
 				</tr>
 			</tbody>
+			<tfoot v-if="multiBand">
+				<tr>
+					<td :colspan="cols">
+						<input type="checkbox" v-model="isGray" /> Grayscale
+					</td>
+				</tr>
+			</tfoot>
 		</table>
 	</div>
 </template>
@@ -40,8 +47,14 @@ export default {
 		}
 	},
 	computed: {
-		isGray() {
-			return this.bands.length === 1;
+		multiBand() {
+			return this.bands.length > 1;
+		},
+		cols() {
+			if (this.isGray) {
+				return this.multiBand ? 3 : 2;
+			}
+			return 4;
 		},
 		labels() {
 			return this.isGray ? ['Grayscale'] : ['Red', 'Green', 'Blue'];
@@ -49,33 +62,49 @@ export default {
 	},
 	data() {
 		return {
-			channels: []
+			channels: [],
+			isGray: this.bands.length === 1
 		}
 	},
 	watch: {
 		bands: {
 			immediate: true,
 			handler() {
-				this.channels = this.bands.slice(0,3).map(band => Object.assign({}, band));
-				// If only two channels are available, add a third one so that we have enough for RGB
-				// If we only have one channel it is grayscale
-				if (this.channels.length === 2) {
-					this.channels.push(Object.assign({}, this.channels[1]));
+				if (this.bands.length === 1) {
+					this.isGray = true;
 				}
+				this.updateChannels();
 			}
+		},
+		isGray() {
+			this.updateChannels();
+			this.emit();
 		},
 		channels: {
 			deep: true,
 			handler() {
-				if (this.channels.filter(c => this.isValid(c.min) && this.isValid(c.max)).length > 0) {
-					this.$emit('update', 'channels', this.channels);
-				}
+				this.emit();
 			}
 		}
 	},
 	methods: {
 		isValid(value) {
 			return (typeof value === 'number');
+		},
+		updateChannels() {
+			this.channels = this.bands
+				.slice(0, this.isGray ? 1 : 3)
+				.map(band => Object.assign({}, band));
+			// If only two channels are available, add a third one so that we have enough for RGB
+			// If we only have one channel it is grayscale
+			if (this.channels.length === 2) {
+				this.channels.push(Object.assign({}, this.channels[1]));
+			}
+		},
+		emit() {
+			if (this.channels.filter(c => this.isValid(c.min) && this.isValid(c.max)).length > 0) {
+				this.$emit('update', 'channels', this.channels);
+			}
 		}
 	}
 }
